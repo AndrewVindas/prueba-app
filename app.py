9d70213e65e44db09fd9dcd1cee68db7c81e8c0a
AVT <ANDREW.VINDAS@ucr.ac.cr>
2025-05-22 01:17:29 -0600

    ajuste de database

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+
 app = Flask(__name__)
-# configuración aquí...
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///local.db')  # fallback local
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 
 with app.app_context():
     db.create_all()
